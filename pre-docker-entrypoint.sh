5909e199f0c360969d604039981bb3d4ed829182
Rasmus Munk <munk1@live.dk>
2021-12-08 16:12:44 +0100

    Add some helpful prints

--- pre-docker-entrypoint.sh
+++ pre-docker-entrypoint.sh
@@ -5,10 +5,11 @@ set -e
 # access to a mounted docker socket if it is set
 # TODO, ensure the correct permissions to execute the following
 # commands via the gosu tool
-echo "Assigning GID $DOCKER_GID"
+echo "Assigning GID $DOCKER_GID to the go user"
 if [ "$DOCKER_GID" ]; then
     groupadd -g $DOCKER_GID hostdocker
     usermod -a -G hostdocker go
 fi
 
+echo "Executing /docker-entrypoint.sh as go"
 gosu go ./docker-entrypoint.sh
